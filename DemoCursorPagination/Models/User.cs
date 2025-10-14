0808ff7eb10787883388ec12a5514ffffba6673d
hoc081098@gmail.com <hoc081098@gmail.com>
2025-10-14 17:31:33 +0700

    init

---
+++ DemoCursorPagination/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DemoCursorPagination.Models;
+
+public partial class User
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<UserNote> UserNotes { get; set; } = new List<UserNote>();
+}
---
+++ DemoCursorPagination/Models/UserNote.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DemoCursorPagination.Models;
+
+public partial class UserNote
+{
+    public Guid Id { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public string Note { get; set; } = null!;
+
+    public DateOnly NoteDate { get; set; }
+
+    public virtual User User { get; set; } = null!;
+}
