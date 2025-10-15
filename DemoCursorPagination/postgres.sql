4d34d233488c73fc7e075b30c26825175d2b7ede
hoc081098@gmail.com <hoc081098@gmail.com>
2025-10-16 02:16:50 +0700

    Add PostgreSQL schema and table definitions for user notes functionality

---
+++ DemoCursorPagination/postgres.sql
@@ -0,0 +1,34 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE SCHEMA IF NOT EXISTS identity;
+CREATE SCHEMA IF NOT EXISTS notes;
+
+DROP TABLE IF EXISTS "identity"."users";
+-- Table Definition
+CREATE TABLE "identity"."users" (
+    "id" uuid NOT NULL DEFAULT gen_random_uuid(),
+    "name" varchar(20) NOT NULL,
+    PRIMARY KEY ("id")
+);
+
+DROP TABLE IF EXISTS "notes"."user_notes";
+-- Table Definition
+CREATE TABLE "notes"."user_notes" (
+    "id" uuid NOT NULL DEFAULT gen_random_uuid(),
+    "user_id" uuid NOT NULL,
+    "note" varchar(500) NOT NULL,
+    "note_date" date NOT NULL DEFAULT CURRENT_DATE,
+    PRIMARY KEY ("id")
+);
+
+ALTER TABLE notes.user_notes
+  ADD CONSTRAINT fk_user_notes__user
+  FOREIGN KEY (user_id) REFERENCES identity.users(id)
+  ON DELETE CASCADE;
+
+-- Indices
+CREATE INDEX IF NOT EXISTS idx_user_notes_user_id
+  ON notes.user_notes (user_id);
+
+CREATE INDEX IF NOT EXISTS idx_user_notes_note_date_id
+  ON notes.user_notes (note_date DESC, id DESC);
