9226be6b4d8c07f46fdb86d85fe609f0d2c8ca5d
hoc081098@gmail.com <hoc081098@gmail.com>
2025-10-15 17:26:43 +0700

    Add NSwag for API documentation and implement note retrieval endpoint

--- DemoCursorPagination/Program.cs
+++ DemoCursorPagination/Program.cs
@@ -11,7 +11,43 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
   options.EnableSensitiveDataLogging(builder.Environment.IsDevelopment());
 });
 
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(config =>
+{
+  config.DocumentName = "DemoCursorPaginationAPI";
+  config.Title = "DemoCursorPaginationAPI v1";
+  config.Version = "v1";
+});
+
 var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseOpenApi();
+    app.UseSwaggerUi(config =>
+    {
+        config.DocumentTitle = "DemoCursorPaginationAPI";
+        config.Path = "/swagger";
+        config.DocumentPath = "/swagger/{documentName}/swagger.json";
+        config.DocExpansion = "list";
+    });
+}
+
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/noteitems", async (ApplicationDbContext db) =>
+{
+  var items = await db.UserNotes
+    .AsNoTracking()
+    .Select(x => new {
+        x.Id,
+        x.Note,
+        x.NoteDate,
+        x.UserId,
+        UserName = x.User.Name
+    })
+    .ToListAsync();
+  return items;
+});
+
 app.Run();
 
